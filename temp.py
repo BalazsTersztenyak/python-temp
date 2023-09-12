22b0e0443150fd2e5f0d25b4730f4a35e64cb475
BalazsTersztenyak <balazs.tersztenyak@stud.uni-obuda.hu>
2023-09-12 11:27:59 +0200

    update temp.py

--- temp.py
+++ temp.py
@@ -1,7 +1,29 @@
 #! /usr/bin/python
+import time
+import os
 
 TEMP_PATH="/sys/class/thermal/thermal_zone0/"
 
-fo = open(TEMP_PATH + "temp","r")
-temp=fo.read()
-print(temp)
+#fo = open(TEMP_PATH + "temp","r")
+#temp=fo.read()
+#print(temp)
+
+def readTemp():
+    fo = open(TEMP_PATH + "temp", "r")
+    temp = fo.read()
+    return int(temp)*0.001
+
+
+
+def avgTemp(n):
+    avg = 0
+    for i in range(n):
+        avg += readTemp()
+    avg /= n
+    return avg
+
+def continous():
+    while True:
+        os.system("clear")
+        print(avgTemp(40),"°C")
+        time.sleep(1)
